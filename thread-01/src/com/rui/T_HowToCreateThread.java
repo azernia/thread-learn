0ae343c2e627baf0b266b154d7bc3a35230f2047
Rui <872715188@qq.com>
2022-05-06 21:46:21 +0800

    创建线程的两种方式

---
+++ thread-01/src/com/rui/T_HowToCreateThread.java
@@ -0,0 +1,24 @@
+package com.rui;
+
+public class T_HowToCreateThread {
+    static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("hello my thread");
+        }
+    }
+    static class MyRun implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("hello my run");
+        }
+    }
+    public static void main(String[] args) {
+        new MyThread().start();
+        new Thread(new MyRun()).start();
+        new Thread(() -> {
+            System.out.println("hello lambda");
+        }).start();
+    }
+}
