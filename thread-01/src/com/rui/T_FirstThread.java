6260ce1949575a8c647fc785880ebbf7fd4ece30
rui <azernia@foxmail.com>
2022-05-06 16:54:54 +0800

    第一次提交

---
+++ thread-01/src/com/rui/T_FirstThread.java
@@ -0,0 +1,35 @@
+package com.rui;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author rui
+ * @since 2022/5/6 16:50
+ */
+public class T_FirstThread {
+    private static class T1 extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    TimeUnit.MICROSECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("T1");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new T1().start();
+        for (int i = 0; i < 10; i++) {
+            try {
+                TimeUnit.MICROSECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("main");
+        }
+    }
+}
