e47aafeb91e7c44c1f5a1d3b7d2c2d0eee7cf44c
rui <azernia@foxmail.com>
2022-05-19 09:47:48 +0800

    Semaphore

---
+++ thread-01/src/com/rui/newLock/SemaphoreDemo.java
@@ -0,0 +1,42 @@
+package com.rui.newLock;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Semaphore也叫信号量，在JDK1.5被引入，可以用来控制同时访问特定资源的线程数量，通过协调各个线程，以保证合理的使用资源。
+ * 应用场景 卖票
+ * create date 2022/5/19 09:09
+ *
+ * @author rui
+ */
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        // 最多允许多少个线程同时执行
+        Semaphore semaphore = new Semaphore(1);
+
+        new Thread(() -> {
+            try {
+                semaphore.acquire();    // 取得 -》 permits - 1
+                System.out.println("T1 running ....");
+                Thread.sleep(200);
+                System.out.println("T1 running ....");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                semaphore.acquire();    // 取得 -》 permits - 1
+                System.out.println("T2 running ....");
+                Thread.sleep(200);
+                System.out.println("T2 running ....");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
+        }).start();
+    }
+}
