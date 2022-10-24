aacc6afb6314c23034799cd006a4596857daf1ea
rui <azernia@foxmail.com>
2022-10-24 11:29:46 +0800

    feat: thread common method

--- thread-heima/src/main/java/com/rui/threadcommonmethod/TCM_06_Interrupt.java
+++ thread-heima/src/main/java/com/rui/threadcommonmethod/TCM_06_Interrupt.java
@@ -3,6 +3,8 @@ package com.rui.threadcommonmethod;
 import com.rui.utils.Sleeper;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.locks.LockSupport;
+
 /**
  * description: 打断线程
  * <hr/>
@@ -12,13 +14,32 @@ import lombok.extern.slf4j.Slf4j;
  * 错误思路
  * 1. 使用 stop，会真正的杀死线程，导致线程无法释放被锁住的共享资源
  * 2. 使用 exit，会使整个程序终止运行
+ * 3. suspend 暂停线程 无法释放锁
+ * 4. resume 恢复线程 破坏同步代码块
  *
  * @author rui
  */
 @Slf4j(topic = "rui.TCM_06_Interrupt")
 public class TCM_06_Interrupt {
     public static void main(String[] args) {
-        test2();
+        test3();
+    }
+
+    /**
+     * 打断 park 线程
+     * 不会清空打断状态
+     */
+    public static void test3() {
+        Thread t1 = new Thread(() -> {
+            log.debug("park");
+            LockSupport.park(); // 当打断标记为真的时候会失效
+            log.debug("un park");
+            log.debug("interrupt status: {}", Thread.currentThread().isInterrupted());
+            log.debug("reset interrupt tag: {}", Thread.interrupted());
+        }, "t1");
+        t1.start();
+        Sleeper.sleep(1);
+        t1.interrupt();
     }
 
 
