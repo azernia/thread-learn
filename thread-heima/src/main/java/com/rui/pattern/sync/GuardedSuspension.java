a0da1b18d0f6c08d2cd1ec8d1b67f6bc3f3b0cca
rui <azernia@foxmail.com>
2022-10-25 17:51:38 +0800

    feat: GuardedSuspension

---
+++ thread-heima/src/main/java/com/rui/pattern/sync/GuardedSuspension.java
@@ -0,0 +1,14 @@
+package com.rui.pattern.sync;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * description: 同步模式之保护性暂停
+ * <hr/>
+ * date: 2022/10/25
+ *
+ * @author rui
+ */
+@Slf4j(topic = "rui.GuardedSuspension")
+public class GuardedSuspension {
+}
