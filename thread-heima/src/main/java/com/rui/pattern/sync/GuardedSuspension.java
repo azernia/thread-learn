509b792ddc47ceed86f9330abe3b51e12956c36c
Rui <872715188@qq.com>
2022-10-27 21:39:24 +0800

    feat: GuardedSuspension

--- thread-heima/src/main/java/com/rui/pattern/sync/GuardedSuspension.java
+++ thread-heima/src/main/java/com/rui/pattern/sync/GuardedSuspension.java
@@ -1,6 +1,10 @@
 package com.rui.pattern.sync;
 
+import com.rui.utils.DownLoader;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * description: 同步模式之保护性暂停
@@ -13,7 +17,24 @@ import lombok.extern.slf4j.Slf4j;
 public class GuardedSuspension {
 
     public static void main(String[] args) {
+        GuardedObj guardedObj = new GuardedObj();
         // 线程1 等待 线程2 的下载结果
+        new Thread(() -> {
+            // 等待结果
+            log.debug("等待结果");
+            List<String> list = (List<String>) guardedObj.get();
+            log.debug("结果大小：{}", list.size());
+        }, "t1").start();
+
+        new Thread(() -> {
+            log.debug("执行下载");
+            try {
+                List<String> download = DownLoader.download();
+                guardedObj.complete(download);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }, "t2").start();
     }
 
 }
--- thread-heima/src/main/java/com/rui/utils/DownLoader.java
+++ thread-heima/src/main/java/com/rui/utils/DownLoader.java
@@ -2,8 +2,13 @@ package com.rui.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,10 +23,16 @@ import java.util.List;
 public class DownLoader {
     private DownLoader() {}
 
-    public static List<String> download() {
+    public static List<String> download() throws IOException {
         HttpURLConnection connection = (HttpURLConnection) new URL("https://www.baidu.com").openConnection();
         List<String> lines = new ArrayList<>();
-
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        return lines;
     }
 
 }
