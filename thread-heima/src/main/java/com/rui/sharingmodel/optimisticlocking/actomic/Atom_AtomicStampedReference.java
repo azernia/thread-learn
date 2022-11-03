2698ba00a782226c975f79447b4dae47415b06a7
rui <azernia@foxmail.com>
2022-11-03 10:26:14 +0800

    feat: atomic stamped reference

--- thread-heima/src/main/java/com/rui/sharingmodel/optimisticlocking/actomic/Atom_AtomicStampedReference.java
+++ thread-heima/src/main/java/com/rui/sharingmodel/optimisticlocking/actomic/Atom_AtomicStampedReference.java
@@ -1,9 +1,10 @@
 package com.rui.sharingmodel.optimisticlocking.actomic;
 
-import com.rui.utils.Sleeper;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.atomic.AtomicStampedReference;
+
+import static com.rui.utils.Sleeper.sleep;
 
 /**
  * description: 处理 ABA 问题
@@ -15,28 +16,32 @@ import java.util.concurrent.atomic.AtomicStampedReference;
 @Slf4j(topic = "rui.Atom_AtomicStampedReference")
 public class Atom_AtomicStampedReference {
 
-    private static final AtomicStampedReference<String> REFERENCE = new AtomicStampedReference<>("A", 0);
+    static AtomicStampedReference<String> ref = new AtomicStampedReference<>("A", 0);
 
-    public static void main(String[] args) {
-        log.debug("main start");
-        String prev = REFERENCE.getReference();
-        int version = REFERENCE.getStamp();
-        log.debug("version is {}", version);
-        other(version);
-        Sleeper.sleep(1);
-        log.debug("change A -> C {}, version is {}", REFERENCE.compareAndSet(prev, "C", version, version + 1), REFERENCE.getStamp());
+    public static void main(String[] args) throws InterruptedException {
+        log.debug("main start...");
+        // 获取值 A
+        String prev = ref.getReference();
+        // 获取版本号
+        int stamp = ref.getStamp();
+        log.debug("版本 {}", stamp);
+        // 如果中间有其它线程干扰，发生了 ABA 现象
+        other();
+        sleep(1);
+        // 尝试改为 C
+        log.debug("change A->C {}", ref.compareAndSet(prev, "C", stamp, stamp + 1));
     }
 
-    private static void other(int version) {
+    private static void other() {
         new Thread(() -> {
-            log.debug("change A -> B {}, version is {}", REFERENCE.compareAndSet(REFERENCE.getReference(), "B", version, version + 1), REFERENCE.getStamp());
+            log.debug("change A->B {}", ref.compareAndSet(ref.getReference(), "B", ref.getStamp(), ref.getStamp() + 1));
+            log.debug("更新版本为 {}", ref.getStamp());
         }, "t1").start();
-        Sleeper.sleep(0.5);
+        sleep(0.5);
         new Thread(() -> {
-            log.debug("change B -> A {}, version is {}", REFERENCE.compareAndSet(REFERENCE.getReference(), "A", version, version + 1), REFERENCE.getStamp());
+            log.debug("change B->A {}", ref.compareAndSet(ref.getReference(), "A", ref.getStamp(), ref.getStamp() + 1));
+            log.debug("更新版本为 {}", ref.getStamp());
         }, "t2").start();
     }
-
-
 
 }
